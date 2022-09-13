e8c6e1b3055be937506058359808f4cc5c0ed167
Jisheng Zhang <jszhang@kernel.org>
2022-09-13 20:44:26 +0800

    Add + and - operators

--- main.c
+++ main.c
@@ -7,9 +7,31 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf("  .globl main\n");
   printf("main:\n");
-  printf("  ldr x0, =%d\n", atoi(argv[1]));
+  printf("  ldr x0, =%ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  ldr x1, =%ld\n", strtol(p, &p, 10));
+      printf("  add x0, x0, x1\n");
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  ldr x1, =%ld\n", strtol(p, &p, 10));
+      printf("  sub x0, x0, x1\n");
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
