55da189d4243874ff527a3e06a3eca05627b6e66
Jisheng Zhang <jszhang@kernel.org>
2022-09-21 21:57:51 +0800

    Support function definition up to 6 parameters

--- codegen.c
+++ codegen.c
@@ -196,6 +196,11 @@ void codegen(Function *prog) {
     printf("  mov x29, sp\n");
     printf("  sub sp, sp, #%d\n", fn->stack_size);
 
+    // Save passed-by-register arguments to the stack
+    int i = 0;
+    for (Obj *var = fn->params; var; var = var->next)
+      printf("  str %s, [x29, #%d]\n", argreg[i++], var->offset);
+
     // Emit code
     gen_stmt(fn->body);
     assert(depth == 0);
